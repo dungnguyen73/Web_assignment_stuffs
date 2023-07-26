26a32d5dec8a225bdfdb30b6507a2a72c4ee6273
dungnguyen73 <91122031+dungnguyen73@users.noreply.github.com>
2023-07-26 20:14:33 +0700

    Add files via upload
    upload navigator bar and footer (first commit)

---
+++ NavAndFooter/footer.php
@@ -0,0 +1,171 @@
+    <!-- footer section -->
+    <!-- Footer -->
+    <footer class="bg text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Facebook -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-facebook-f"></i
+        ></a>
+  
+        <!-- Twitter -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-twitter"></i
+        ></a>
+  
+        <!-- Google -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-google"></i
+        ></a>
+  
+        <!-- Instagram -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-instagram"></i
+        ></a>
+  
+        <!-- Linkedin -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-linkedin-in"></i
+        ></a>
+  
+        <!-- Github -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-github"></i
+        ></a>
+      </section>
+      <!-- Section: Social media -->
+  
+      <!-- Section: Form -->
+      <section class="">
+        <form action="#">
+          <!--Grid row-->
+          <div class="row d-flex justify-content-center">
+            <!--Grid column-->
+            <div class="col-auto">
+              <p class="pt-2">
+                <strong>Sign up for our newsletter</strong>
+              </p>
+            </div>
+            <!--Grid column-->
+  
+            <!--Grid column-->
+            <div class="col-md-5 col-12">
+              <!-- Email input -->
+              <div class="form-outline form-white mb-4">
+                <input type="email" id="form5Example21" class="form-control" placeholder="Email" />
+                <!-- <label class="form-label" for="form5Example21">Email address</label> -->
+              </div>
+            </div>
+            <!--Grid column-->
+  
+            <!--Grid column-->
+            <div class="col-auto">
+              <!-- Submit button -->
+              <button type="submit" class="btn btn-outline-light mb-4">
+                Subscribe
+              </button>
+            </div>
+            <!--Grid column-->
+          </div>
+          <!--Grid row-->
+        </form>
+      </section>
+      <!-- Section: Form -->
+  
+      <!-- Section: Text -->
+      <section class="mb-4">
+        <p>
+          
+        </p>
+      </section>
+      <!-- Section: Text -->
+  
+      <!-- Section: Links -->
+      <section class="">
+        <!--Grid row-->
+        <div class="row">
+          <!--Grid column-->
+          <div class="col-lg-7 col-md-5 mb-4 mb-md-0" id="first-col">
+            <!-- <h5 class="text-uppercase">Links</h5> -->
+  
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white text-uppercase">Find a store</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white text-uppercase">Become a member</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Send Us Feedback</a>
+              </li>
+              
+            </ul>
+          </div>
+          <!--Grid column-->
+        
+          <!--Grid column-->
+          <div class="col-6 col-md-2 mb-3 infor">
+            <h5 class="text-uppercase">Get help</h5>
+  
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" >Order Status</a>
+              </li>
+              <li>
+                <a href="#!" >Delivery</a>
+              </li>
+              <li>
+                <a href="#!" >Returns</a>
+              </li>
+              <li>
+                <a href="#!" >Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-6 col-md-2 mb-3 infor">
+            <h5 class="text-uppercase">About Nike</h5>
+  
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" >News</a>
+              </li>
+              <li>
+                <a href="#!" >Careers</a>
+              </li>
+              <li>
+                <a href="#!" >Investors</a>
+              </li>
+              <li>
+                <a href="#!" >Sustainability</a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+  
+          
+  
+          
+          
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+    </div>
+    <!-- Grid container -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2023 Copyright: Developed by TDung
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+
+
+
+    <!-- end footer section -->
---
+++ NavAndFooter/nav.php
@@ -0,0 +1,48 @@
+<!-- navigator bar section -->
+
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand"><img src="assets/Logo_NIKE.svg.png" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsebtn" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class = "collapse navbar-collapse d-lg-flex justify-content-center" id = "collapsebtn">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link " >Shoes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link ">
+                            Clothing
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link ">
+                            Accessories
+                        </a>
+                    </li>
+                </ul>
+                <div class= "d-md-none options">
+                    <i class="fa-sharp fa-regular fa-heart fa-lg" title="Favorites"></i>
+                    <i class="fa-sharp fa-solid fa-bag-shopping  fa-lg" title= "Bag items"></i>
+                    <i class="fa-sharp fa-regular fa-user  fa-lg" title = "User"></i>
+                </div>
+            </div>
+            
+            <div class="d-none d-md-block" id = "searchbar">
+                <form class="form-inline">
+                  <input class="form-control mr-md-2" type="search" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+            
+            <div class= "d-none d-md-block options" >
+                <i class="none fa-sharp fa-regular fa-heart fa-lg" title="Favorites"></i>
+                <i class="fa-sharp fa-solid fa-bag-shopping fa-lg" title= "Bag items"></i>
+                <i class="fa-sharp fa-regular fa-user fa-lg" title = "User"></i>
+            </div>
+
+            
+
+        </div>
+    </nav>    
+        
