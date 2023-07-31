ad01501b82ac3a4631f62224cfc9f223795b73f1
dungnguyen73 <91122031+dungnguyen73@users.noreply.github.com>
2023-07-31 23:05:27 +0700

    Add files via upload
    add homepage in file main.php

--- NavAndFooter/footer.php
+++ NavAndFooter/footer.php
@@ -169,3 +169,6 @@
 
 
     <!-- end footer section -->
+
+    <script src="https://kit.fontawesome.com/a1037a3a28.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
